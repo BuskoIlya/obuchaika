7247ea99ccde9eabe631e481c3416863ad70d04f
Busko Ilya <trassae95st@mail.ru>
2023-11-25 18:01:19 +0300

    Refactor menu items initialization

--- matematika/main.cpp
+++ matematika/main.cpp
@@ -3,46 +3,43 @@
 
 #include "menu.hpp"
 #include "menu_functions.hpp"
+#include "menu_items.hpp"
 
 int main() {
     std::setlocale(LC_ALL, "");
 
-    IBusko::MenuItem study_summ = { "1 - Хочу научиться складывать!", IBusko::study_summ };
-    IBusko::MenuItem study_substract = { "2 - Хочу научиться вычитать!", IBusko::study_substract };
-    IBusko::MenuItem study_multiply = { "3 - Хочу научиться умножать!", IBusko::study_multiply };
-    IBusko::MenuItem study_divide = { "4 - Хочу научиться делить!", IBusko::study_divide };
-    IBusko::MenuItem study_go_back = { "0 - Выйти в главное меню", IBusko::study_go_back };
-
-    IBusko::MenuItem* study_children[] = { 
-        &study_go_back,
-        &study_summ,
-        &study_substract,
-        &study_multiply,
-        &study_divide
-    };
-    const int study_size = sizeof(study_children) / sizeof(study_children[0]);
-
-    IBusko::MenuItem study = { "1 - Хочу учиться математике!", IBusko::show_menu, study_children, study_size };
-    IBusko::MenuItem exit = { "0 - Я лучше пойду полежу...", IBusko::exit };
-
-    IBusko::MenuItem* main_children[] = { &exit, &study };
-    const int main_size = sizeof(main_children) / sizeof(main_children[0]);
-
-    IBusko::MenuItem main = { nullptr, IBusko::show_menu, main_children, main_size };
-
-    study_summ.parent = &study;
-    study_substract.parent = &study;
-    study_multiply.parent = &study;
-    study_divide.parent = &study;
-    study_go_back.parent = &study;
-
-    study.parent = &main;
-    exit.parent = &main;
-
-    const IBusko::MenuItem* current = &main;
+    const IBusko::MenuItem* current = &IBusko::MAIN;
     do {
         current = current->func(current);
     } while (true);
 
     return 0;
 }
+
+/*
+    Задание.
+
+    I. Разобрать программу по шагам и реализовать её на своих машинах вместе с гитом.
+    Залить всё на гитхаб.
+
+    II. Увеличить меню до 3-х уровней:
+        1-ый уровень: 
+                      1 - Хочу учиться математике!
+                      0 - Я лучше пойду полежу...
+        2-ой уровень для "1 - Хочу учиться математике!": 
+                      1 - Хочу изучать алгебру!
+                      2 - Хочу изучать математический анализ!
+                      0 - Выйти в главное меню.
+        
+        3-ий уровень для "1 - Хочу изучать алгебру!":
+                      1 - Хочу научиться складывать!
+                      2 - Хочу научиться вычитать!
+                      3 - Хочу научиться умножать!
+                      4 - Хочу научиться делить!
+                      0 - Вернуться к выбору предметов.
+
+        3-ий уровень для "Хочу изучать математический анализ!":
+                      1 - Хочу изучить дифференциальное исчисление!
+                      2 - Хочу изучить интегральное исчисление!
+                      0 - Вернуться к выбору предметов.
+*/
--- matematika/menu.hpp
+++ matematika/menu.hpp
@@ -5,9 +5,9 @@ namespace IBusko {
         const char* const title;
         const MenuItem* (*func)(const MenuItem* current);
 
+        const MenuItem* parent;
+
         const MenuItem* const *children;
         const int children_count;
-
-        const MenuItem* parent;
     };
 }
